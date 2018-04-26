adef39f8e9ade87986c6fd6e4590027e1f8a8d34
Matthew Rizzini <matthewrizzini@gmail.com>
2018-04-26 18:30:16 -0400

    testing c

---
+++ Homework.cs
@@ -0,0 +1,23 @@
+using System;
+namespace Bulldozer
+{
+    class MainClass 
+    {
+        public static void Main(string())??? 
+        {
+	var dozer = new BullDozer
+	dozer.color = “Yellow”
+	dozer.size = “Mini”
+            Console.WriteLine(“Bulldozer is “ + dozer.color);
+
+        }
+    }
+}
+
+
+
+public class BullDozer
+{
+	public string color { get: set: }
+	public string size { get: set }
+}
